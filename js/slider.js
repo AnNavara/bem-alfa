eb3ff45db18eaf92e2eb91299abff0f6ca96b80a
AnNavara <navarachkou@gmail.com>
2015-08-05 08:23:07 +0500

    Fixing main page sliders
    also adding estate page pics

--- js/slider.js
+++ js/slider.js
@@ -4,44 +4,59 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $(".about-slider__wrapper").slick({
-		dots: false,
+		dots: true,
+		dotsClass: "about-slider__btns",
 		arrows: true,
 		prevArrow: false,
 		nextArrow: $(".about-slider__next"),
 		infinite: true,
 		speed: 600,
-		slidesToShow: 2,
+		slidesToShow: 1,
 		centerMode: false,
 		variableWidth: true
   });
 });
 
-$(".offer__wrapper").slick({
-  dots: false,
-	prevArrow: $(".offer__btn--left"),
-	nextArrow: $(".offer__btn--right"),
-	draggable: false,
-  infinite: false,
-  speed: 600,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1120,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      }
-    }
-    // {
-    //   breakpoint: 920,
-    //   settings: {
-    //     slidesToShow: 3,
-    //     slidesToScroll: 3
-    //   }
-    // }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+$(document).ready(function(){
+	$(".js-spec__slide").slick({
+	  dots: false,
+		prevArrow: $(".js-spec__btn--l"),
+		nextArrow: $(".js-spec__btn--r"),
+		draggable: false,
+	  infinite: false,
+	  speed: 600,
+	  slidesToShow: 4,
+	  slidesToScroll: 4,
+	  responsive: [
+	    {
+	      breakpoint: 1120,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	      }
+	    }
+	  ]
+	});
+});
+
+$(document).ready(function(){
+	$(".js-3d__slide").slick({
+	  dots: false,
+		prevArrow: $(".js-3d__btn--l"),
+		nextArrow: $(".js-3d__btn--r"),
+		draggable: false,
+	  infinite: false,
+	  speed: 600,
+	  slidesToShow: 4,
+	  slidesToScroll: 4,
+	  responsive: [
+	    {
+	      breakpoint: 1120,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	      }
+	    }
+	  ]
+	});
 });
