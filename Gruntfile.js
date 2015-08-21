b75574d838e0fe08867b9e3c601e197c07514df6
AnNavara <navarachkou@gmail.com>
2015-08-21 16:11:14 +0500

    Fixing catalog padding and new-buildings link positioning

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,138 +1,139 @@
 module.exports = function(grunt) {
 
-	require("load-grunt-tasks")(grunt);
+  require("load-grunt-tasks")(grunt);
 
-	grunt.initConfig({
-		less: {
-			style: {
-				files:{
-					"build/css/style.css": ["source/less/style.less"]
-				}
-			}
-		},
+  grunt.initConfig({
+    less: {
+      style: {
+        files:{
+          "build/css/style.css": ["source/less/style.less"]
+        }
+      }
+    },
 
-		autoprefixer: {
-			options: {
-				browsers: ["last 3 version", "ie 9"]
-			},
-			style: {
-				src: "build/css/style.css"
-			}
-		},
+    autoprefixer: {
+      options: {
+        browsers: ["last 3 version", "ie 9"]
+      },
+      style: {
+        src: "build/css/style.css"
+      }
+    },
 
-		cmq: {
-			style: {
-				files: {
-					"build/css/style.css": ["build/css/style.css"]
-				}
-			}
-		},
+    cmq: {
+      style: {
+        files: {
+          "build/css/style.css": ["build/css/style.css"]
+        }
+      }
+    },
 
-		csscomb: {
-			less: {
-				expand: true,
-				src: ["less/**/*.less"],
-			},
-			css: {
-				expand: true,
-				src: ["css/**/style.css"],
-			}
-		},
+    csscomb: {
+      less: {
+        expand: true,
+        src: ["source/less/**/*.less"],
+      },
+      css: {
+        expand: true,
+        src: ["build/css/**/style.css"],
+      }
+    },
 
-		watch: {
-			less: {
-				files: "less/**/*.less",
-				tasks: ["less"],
-				option: {
-					livereload: true,
-				},
-			},
-		},
+    watch: {
+      less: {
+        files: "less/**/*.less",
+        tasks: ["less"],
+        option: {
+          livereload: true,
+        },
+      },
+    },
 
-		imagemin: {
-			images: {
-				options: {
-					optimizationLevel: 3
-				},
-				files: [{
-					expand: true,
-					src: ["img/**/*.{png, jpg, svg}"]
-				}]
-			}
-		},
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["img/**/*.{png, jpg, svg}"]
+        }]
+      }
+    },
 
-		cssmin: {
-			style: {
-				options: {
-					keepSpecialCommnets: 0,
-					report: "gzip"
-				},
-				files: {
-					"build/css/style.min.css": ["build/css/style.css"]
-				}
-			}
-		},
+    cssmin: {
+      style: {
+        options: {
+          keepSpecialCommnets: 0,
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
 
-		copy: {
-			build: {
-				files: [{
-					expand: true,
-					cwd: "source",
-					src: [
-						"img/**",
-						"js/**",
-						"*.html"
-					],
-					dest: "build"
-				}]
-			}
-		},
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "img/**",
+            "js/**",
+            "*.html"
+          ],
+          dest: "build"
+        }]
+      }
+    },
 
-		clean: {
-			build: ["build"]
-		},
+    clean: {
+      build: ["build"]
+    },
 
-		replace: {
-			build: {
-				options: {
-					patterns: [{
-						match: /[\"\']img\//g,
-						replacement: '"/img/'
-					}, {
-						match: /[\"\']css\//g,
-						replacement: '"/css/'
-					}, {
-						match: /[\"\']js\//g,
-						replacement: '"/js/'
-					}]
-				},
-				files: [{
-					expand: true,
-					src: [
-						"build/css/style*.css",
-						"build/*.html"
-					]
-				}]
-			}
-		},
+    replace: {
+      build: {
+        options: {
+          patterns: [{
+            match: /[\"\']img\//g,
+            replacement: '"/img/'
+          }, {
+            match: /[\"\']css\//g,
+            replacement: '"/css/'
+          }, {
+            match: /[\"\']js\//g,
+            replacement: '"/js/'
+          }]
+        },
+        files: [{
+          expand: true,
+          src: [
+            "build/css/style*.css",
+            "build/*.html"
+          ]
+        }]
+      }
+    },
 
-		sprite: {
-			all: {
-				src: "img/_source-icon/*.png",
-				dest: "img/sprite.png",
-				destCss: "less/components/sprite.less"
-			}
-		}
+    sprite: {
+      all: {
+        src: "img/_source-icon/*.png",
+        dest: "img/sprite.png",
+        destCss: "less/components/sprite.less"
+      }
+    }
 
-	});
+  });
 
-		grunt.registerTask("build", [
-			"clean",
-			"copy",
-			"csscomb",
-			"less",
-			"autoprefixer",
-			"cmq",
-			"cssmin"
-		]);
+    grunt.registerTask("build", [
+      "clean",
+      "copy",
+      "csscomb",
+      "less",
+      "autoprefixer",
+      "cmq",
+      "csscomb",
+      "cssmin"
+    ]);
 };
